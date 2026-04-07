a5b368b390977bc4461e8273a435f94d7e6be6de
hamim-87 <hamim12109087@gmail.com>
2026-04-08 02:01:31 +0600

    frontend config change

--- fontend/next.config.js
+++ fontend/next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['localhost', 'via.placeholder.com', 'picsum.photos', 'images.unsplash.com'],
+    domains: [
+      'localhost',
+      'via.placeholder.com',
+      'picsum.photos',
+      'images.unsplash.com',
+      'auction-api-5lfe.onrender.com',
+      'lmthclkskkechzyfenuf.supabase.co'
+    ],
     unoptimized: true
   }
 }
